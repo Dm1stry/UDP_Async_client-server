a3f41805eeb65db419fcdb20afc1ef7468e7c950
Dm1stry <BattleshipWriter@yandex.ru>
2023-12-28 20:01:21 +0300

    Client complete

--- src/client/main_client.cpp
+++ src/client/main_client.cpp
@@ -1,13 +1,44 @@
 #include <iostream>
-
+#include <string>
+#include <stdlib.h>
 #include "tcpclient.hpp"
+#include "udpclient.hpp"
+
+BaseClient * create_client(std::string con_type)
+{
+    BaseClient * client = nullptr;
+
+    if(con_type == "TCP")
+    {
+        client = new TCPClient();
+    }
+    else if(con_type == "UDP")
+    {
+        client = new UDPClient();
+    }
+    else
+    {
+        std::cerr << "Unknown protocol" << '\n';
+    }
+
+    return client;
+}
 
 int main(int argc, char * argv[])
 {
-    TCPClient client;
-    client.setHost("127.0.0.1");
-    client.setHostPort(49160);
-    client.setPort(49161);
-    std::cout << "Client started!\n";
-    client.run();
+    if(argc < 5)
+    {
+        std::cout << "Usage:\n ./Server [UDP/TCP] [ip] [dest_port] [source_port]\n";
+    }
+    else
+    {
+        BaseClient * client = create_client(argv[1]);
+        std::string ip = argv[2];
+        client->setHost(ip);
+        uint dest_port = atoi(argv[3]);
+        client->setHostPort(dest_port);
+        uint source_port = atoi(argv[4]);
+        client->setPort(source_port);
+        client->run();
+    }
 }
